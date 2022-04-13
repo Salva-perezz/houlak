f602b4b85976423e04910ca2ed24fc5ccae35937
Salva-perezz <69496754+Salva-perezz@users.noreply.github.com>
2022-04-13 12:01:09 -0300

    now the app handle backend errors

--- front/src/components/home/Home.jsx
+++ front/src/components/home/Home.jsx
@@ -1,33 +1,38 @@
 import { useSelector } from "react-redux";
-import Spinner from 'react-bootstrap/Spinner' 
+import Spinner from "react-bootstrap/Spinner";
 import ArtistCard from "./artistCard";
 import AlbumsCard from "./albumsCard";
-import './home.css';
+import "./home.css";
 
- const Home = () => {
-    const artistAndAlbums = useSelector(state => state.artistAndAlbums);
-    const loading = useSelector(state => state.loading);
-    
-    return (
-        <>
-        {loading ? (
-          <div className="loadingContainer">
-            <Spinner animation="border" variant="success" />
+const Home = () => {
+  const artistAndAlbums = useSelector((state) => state.artistAndAlbums);
+  const loading = useSelector((state) => state.loading);
+  const responseError = useSelector((state) => state.responseError);
+
+  return (
+    <>
+      {loading ? (
+        <div className="loadingContainer">
+          <Spinner animation="border" variant="success" />
+        </div>
+      ) : responseError ? (
+        <h1 className="h1Home">{responseError}</h1>
+      ) : artistAndAlbums ? (
+        <div className="artistAndAlbumsContainer">
+          <div className="artistAndAlbumsSubContainer">
+            <ArtistCard artist={artistAndAlbums.artist} />
+            {artistAndAlbums.albums.map((album) => (
+              <AlbumsCard album={album} />
+            ))}
           </div>
-        ) : !artistAndAlbums ? (<h1 className="h1Home">Ingresa el nombre de un artista en el buscador</h1>) : (
-          <div className="artistAndAlbumsContainer">
-            <div className="artistAndAlbumsSubContainer">
-              <ArtistCard artist={artistAndAlbums.artist} />
-              {artistAndAlbums.albums.map(album => (
-                <AlbumsCard album={album}/>
-              )
-              )}
-            </div>
-          </div>
-        )
-        }
-        </>
-    );
+        </div>
+      ) : (
+        <h1 className="h1Home">
+          Ingrese el nombre de un artista en el buscador
+        </h1>
+      )}
+    </>
+  );
 };
 
 export default Home;
